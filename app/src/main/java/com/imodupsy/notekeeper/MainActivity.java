a7e4727f987b2bc9485a0add53a98ac03c5708ca
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-06-30 09:27:01 +0100

    Implement save on exit

--- app/src/main/java/com/imodupsy/notekeeper/MainActivity.java
+++ app/src/main/java/com/imodupsy/notekeeper/MainActivity.java
@@ -24,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
     private boolean mIsNewNote;
     private EditText mTextNoteTitle;
     private EditText mTextNoteText;
+    private int mNotePosition;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveNote();
+    }
+
+    private void saveNote() {
+        mNote.setCourse((CourseInfo) mSpinnerCourses.getSelectedItem());
+        mNote.setTitle(mTextNoteTitle.getText().toString());
+        mNote.setText(mTextNoteText.getText().toString());
+    }
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
@@ -68,8 +81,18 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = getIntent();
         int position = intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET);
         mIsNewNote = position == POSITION_NOT_SET;
-        if (!mIsNewNote)
+        if (mIsNewNote) {
+            createNewNote();
+        } else {
             mNote = DataManager.getInstance().getNotes().get(position);
+        }
+
+    }
+
+    private void createNewNote() {
+        DataManager dm = DataManager.getInstance();
+        mNotePosition = dm.createNewNote();
+        mNote = dm.getNotes().get(mNotePosition);
     }
 
     @Override
